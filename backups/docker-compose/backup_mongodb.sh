d7360d03d9575e9ac7efbd3606c9c4ed2e1c2a1d
Daniel Schemp <git@shmp.systems>
2021-10-17 19:28:55 +0200

    added: Backups

---
+++ backups/docker-compose/backup_mongodb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+OP=$1
+DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+DB_DUMP="mongodb_dump.gz"
+
+case $OP in
+  "before_backup")
+    cd $DIR && \
+    echo "** Starting database ..." && \
+    docker-compose up -d <DATABASE_CONTAINER> && \
+    echo "** Creating DB dump ..." && \
+    docker-compose exec -T <DATABASE_CONTAINER> bash -c "mongodump --archive --gzip" | cat > $DB_DUMP && \
+    echo "** Shutting down docker-compose stack ..." && \
+    docker-compose down
+    ;;
+
+  "after_backup")
+    cd $DIR && \
+    echo "** Removing DB dump ..." && \
+    rm $DB_DUMP && \
+    echo "** Booting up docker-compose stack ..." && \
+    docker-compose up -d
+    ;;
+esac
